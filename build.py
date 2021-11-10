49b4519df1bb25b8e862dab877702aeca55674a9
devidwolf <68775926+devidw@users.noreply.github.com>
2021-11-10 20:34:55 +0100

    add status check and html generation

--- build.py
+++ build.py
@@ -1,4 +1,7 @@
 from csv import DictReader
+from datetime import datetime
+import requests
+import os
 
 
 class IpfsBlogs:
@@ -9,29 +12,44 @@ class IpfsBlogs:
         self.set_csv()
         # self.get_adoc_table()
         self.write_adoc_table()
+        self.write_html()
 
     def set_csv(self):
         self.blogs = DictReader(open(self.file_in))
 
+    def is_online(self, url):
+        try:
+            requests.get(url)
+            return '✅'
+        except:
+            return '❌'
+
     def get_adoc_table(self):
-        adoc_table = ('|===\n'
-        '|Name |Domain\n\n'
-        '%s'
-        '|===')
-        print(adoc_table)
+        adoc_table = ('.Online statuses as of %s\n'
+                      '|===\n'
+                      '|Name |Domain |Online?\n\n'
+                      '%s'
+                      '|===')
+        # print(adoc_table)
         adoc_rows = ''
         for blog in self.blogs:
             name = blog['Name']
             domain = blog['Domain']
-            link = f'link:https://ipfs.io/ipns/{domain}[{domain}]'
-            adoc_rows += f'|{name}|{link}\n'
-        return adoc_table % adoc_rows
+            url = f'https://ipfs.io/ipns/{domain}'
+            link = f'link:{url}[{domain}]'
+            online = self.is_online(url)
+            adoc_rows += f'|{name}|{link}|{online}\n'
+        today = datetime.today().strftime('%Y-%m-%d')
+        return adoc_table % (today, adoc_rows)
 
     def write_adoc_table(self):
         with open(self.file_out_tpl, 'r') as f:
             tpl = f.read()
         with open(self.file_out, 'w') as f:
             f.write(tpl % self.get_adoc_table())
+
+    def write_html(self):
+        os.system(f'asciidoctor -o index.html {self.file_out}')
 
 
 ipfs_blogs = IpfsBlogs(
